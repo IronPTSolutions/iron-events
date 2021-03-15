f745656d16f3b4d36d7d95df7aea30e8e875fd5c
Carlos del Prado <carlos.delprado@11paths.com>
2021-03-15 22:40:33 +0000

    feat: event sigle page

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/nav/Navbar';
 import Footer from './components/footer/Footer';
 import Events from './screens/Events';
 import EventForm from './components/events/EventForm';
+import EventDetail from './components/events/EventDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/events" component={Events} />
           <Route exact path="/create-event" component={EventForm} />
+          <Route exact path="/events/:id" component={EventDetail} />
           <Redirect to="/events" />
         </Switch>
       </div>
--- web/src/components/events/EventDetail.js
+++ web/src/components/events/EventDetail.js
@@ -1,14 +1,70 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import moment from 'moment';
+
+import eventsService from '../../services/events-service';
 
 class EventDetail extends Component {
 
-  // This component must receive the event identifier from props.
+  state = {
+    event: null,
+    notFound: false
+  }
 
-  // Iteration 2: retrieve the 'id' event from API and render it.
-  // remember, you need to use the events' service after the first time that the component was rendered.
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    eventsService.get(id)
+      .then(event => this.setState({ event }))
+      .catch(error => {
+        if (error?.response?.status === 404) {
+          this.setState({ notFound: true })
+        } else {
+          console.error(error);
+        }
+      })
+  }
 
   render() {
-    return (<div></div>)
+
+    const { event, notFound } = this.state;
+    
+    if (notFound) {
+      return (<Redirect to="/events"/>)
+    } else if (!event) {
+      return null;
+    } else {
+
+      const { image, title, description, tags, capacity, start, end } = event;
+
+      return (
+        <Fragment>
+          <div className="row row-cols-1 mb-4">
+            <div className="col text-center">
+              <img src={image} alt={title} className="img-fluid" />
+            </div>
+            <div className="col">
+              <h1 className="mt-4 mb-2">{title}</h1>
+              <div className="d-flex flex-row mb-2">
+                <span className="badge rounded-pill bg-info me-2 p-2"><i className="fa fa-users me-1"></i>0 / {capacity}</span>
+                <span className="badge rounded-pill bg-danger me-2 p-2"><i className="fa fa-clock-o me-1"></i>{moment(start).format('llll')} to {moment(end).format('llll')}</span>
+              </div>
+              {description.split('\n').map(p => <p>{p}</p>)}
+            </div>
+            {tags && (
+              <div className="col">
+                {tags.map(tag => <span className="">{<span className="badge rounded-pill bg-secondary me-2">{tag}</span>}</span>)}
+              </div>
+            )}
+          </div>
+          <div className="row">
+            <div className="col">
+              <Link to="/events" className="fw-lighter"><i className="fa fa-angle-left"></i> Back to Events</Link>
+            </div>
+          </div>
+        </Fragment>
+        
+      );
+    }
   }
 }
 
