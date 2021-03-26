4e04ff1a38364dbec15d092119979c678be4217e
Carlos del Prado <carlos.delprado@11paths.com>
2021-03-26 20:19:24 +0000

    feat: event list with hooks

--- web/src/components/events/EventsList.js
+++ web/src/components/events/EventsList.js
@@ -1,31 +1,38 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import EventItem from './EventItem';
 
 import eventsService from '../../services/events-service';
 
-class EventsList extends Component {
+function EventsList() {
 
-  state = {
-    events: []
-  }
+  const [events, setEvents] = useState([]);
 
-  componentDidMount() {
-    eventsService.list()
-      .then(events => this.setState({ events }))
-      .catch(error => console.error(error))
-  }
+  useEffect(() => {
+    // componentDidMount
 
-  render() {
-    const { events } = this.state;
+    async function fetchEvents() {
+      const events = await eventsService.list();
+      if (!isUnmounted) {
+        setEvents(events)
+      }
+    }
 
-    return (
-      <div className="row row-cols-4">
-        {events.map(event => (
-          <div key={event.id} className="col mb-4"><EventItem event={event} /></div>
-        ))}
-      </div>
-    )
-  }
+    let isUnmounted = false;
+    fetchEvents();
+
+    return () => {
+      // componentWillUnmount
+      isUnmounted =  true;
+    }
+  }, []); // no tiene dependencias para volver a ejecutarse
+  
+  return (
+    <div className="row row-cols-4">
+      {events.map(event => (
+        <div key={event.id} className="col mb-4"><EventItem event={event} /></div>
+      ))}
+    </div>
+  )
 }
 
 export default EventsList;
