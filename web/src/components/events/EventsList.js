3aff9bc64306e6de2a1b5ef3eca2159afd2b5a58
Carlos del Prado <carlos.delprado@11paths.com>
2021-03-26 23:02:26 +0000

    feat: events filter

--- web/src/components/events/EventsList.js
+++ web/src/components/events/EventsList.js
@@ -2,37 +2,66 @@ import { useState, useEffect } from 'react';
 import EventItem from './EventItem';
 
 import eventsService from '../../services/events-service';
+import { Fragment } from 'react';
+import EventsFilter from './EventsFilter';
 
-function EventsList() {
+function EventsList({ minSearchChars }) {
 
-  const [events, setEvents] = useState([]);
+  const [state, setState] = useState({
+    events: [],
+    loading: false
+  });
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     // componentDidMount
 
     async function fetchEvents() {
-      const events = await eventsService.list();
+      console.log('Fetching events...');
+      setState(state => ({
+        ...state,
+        loading: true
+      }))
+      const events = await eventsService.list(search);
       if (!isUnmounted) {
-        setEvents(events)
+        setState({
+          events: events,
+          loading: false
+        })
       }
     }
 
     let isUnmounted = false;
-    fetchEvents();
+
+    if (search.length >= minSearchChars || search.length === 0) {
+      fetchEvents();
+    }
 
     return () => {
       // componentWillUnmount
       isUnmounted =  true;
     }
-  }, []); // no tiene dependencias para volver a ejecutarse
+  }, [search, minSearchChars]); // tiene como dependencia el buscador, para que siempre que cambie de valor se ejecute
+
+  const handleSearch = search => setSearch(search);
   
+  const { events, loading } = state;
+
   return (
-    <div className="row row-cols-4">
-      {events.map(event => (
-        <div key={event.id} className="col mb-4"><EventItem event={event} /></div>
-      ))}
-    </div>
+    <Fragment>
+      <EventsFilter className="mb-3" onSearch={handleSearch} loading={loading} />
+      <div className="row row-cols-4">
+        {events.map(event => (
+          <div key={event.id} className="col mb-4"><EventItem event={event} /></div>
+        ))}
+      </div>
+    </Fragment>
+    
   )
+}
+
+EventsList.defaultProps = {
+  minSearchChars: 4
 }
 
 export default EventsList;
